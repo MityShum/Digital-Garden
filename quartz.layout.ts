0fef0b7af49c404684cff53f3fcc06e3c1c31fd1
MityShum <grbest@mail.ru>
2026-07-31 17:51:35 +0300

    style: убрать GitHub из футера, оставить Telegram

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,6 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/MityShum/Digital-Garden",
       Telegram: "https://t.me/goliydovolniy",
     },
   }),
